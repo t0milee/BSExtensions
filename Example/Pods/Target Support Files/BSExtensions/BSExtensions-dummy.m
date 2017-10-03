51db6207e8230c05a33029a8c37751d67d2be6eb
Tomislav Jankovic <tomislav.jankovic@bstorm.co>
2017-10-03 16:59:19 +0200

    Created project

---
+++ Example/Pods/Target Support Files/BSExtensions/BSExtensions-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BSExtensions : NSObject
+@end
+@implementation PodsDummy_BSExtensions
+@end
---
+++ Example/Pods/Target Support Files/Pods-BSExtensions_Tests/Pods-BSExtensions_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BSExtensions_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_BSExtensions_Tests
+@end
